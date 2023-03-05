19fee5b0d2fb9fd896139471c9ef4e1627e66e0b
AssetMendy <asset.mendy@gmail.com>
2023-03-05 20:04:56 +0000

    Add User's game side in index.html

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('click', function (e) {
+    if (document.getElementById('btClick').contains(e.target)) {
+      document.getElementById('centerDIV').style.display = 'block';
+    }
+    
+    if (document.getElementById('btClose').contains(e.target)) {
+      document.getElementById('centerDIV').style.display = 'none';
+    }
+  })    

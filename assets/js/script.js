b17718266e2e0971a8ba35f93fe7fab7e74e5d4b
AssetMendy <asset.mendy@gmail.com>
2023-03-07 22:05:53 +0000

    Add computer logic to return hand gesture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
-window.addEventListener('click', function (e) {
-    if (document.getElementById('btClick').contains(e.target)) {
-      document.getElementById('centerDIV').style.display = 'block';
-    }
-    
-    if (document.getElementById('btClose').contains(e.target)) {
-      document.getElementById('centerDIV').style.display = 'none';
-    }
-  })    
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = document.getElementsByTagName("input");
+
+  for (let button of buttons) {
+      button.addEventListener("click", function() {
+          if (this.getAttribute("data-type") === "reset") {
+              resetGame();
+          } else {
+              let userHand = this.getAttribute("data-type")
+                  startGame(userHand);
+          }
+      })
+  }
+})
+
+
+
+function startGame(userHand) {
+  let botHand = Math.floor((Math.random() * 5) + 1)
+  if (botHand === 1) {
+    botHand = "rock"
+  } else if (botHand === 2) {
+    botHand = "paper"
+  } else if (botHand === 3) {
+    botHand = "scissors"
+  } else if (botHand === 4) {
+    botHand = "lizard"
+  } else {
+    botHand = "spock"
+  }
+  console.log(botHand)
+}
